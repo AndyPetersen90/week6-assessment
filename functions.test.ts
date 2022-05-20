cb1bf1f3479f4c2cdec4ee7cb729c3da19a3e395
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-20 11:40:51 -0600

    part 3 completed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,12 @@
 const {shuffleArray} = require('./utils')
+const {bots} = require('./data')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test("ShuffleArray to be a function", () => {
+        expect(typeof shuffleArray).toBe('function');
+      })
+
+    test('Array response length is 5', () => {
+        expect(shuffleArray(bots)).toHaveLength(10)
+    })
 })
